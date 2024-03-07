b7d81250e2319f9c6b5c92e6e3a21178f664e1be
VDRohit <sapkalrohit110@gmail.com>
2024-03-07 23:11:52 +0530

    "Login, Register and Dashboard component Created"

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [  MatButtonModule,
+              MatIconModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
