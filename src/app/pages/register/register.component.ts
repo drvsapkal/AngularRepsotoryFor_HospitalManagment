9e68d69c06fd47f05b357d9c83b1c2fad8c6542f
VDRohit <sapkalrohit110@gmail.com>
2024-03-11 22:48:38 +0530

    Created Patient Registration appointment form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  patientobj : patient
+
+  constructor(){
+    this.patientobj = new patient();
+  }
+}
+
+export class patient {
+  firsName: string;
+  emailId: string;
+  date: string;
+  city: string;
+  mobileNumber: number;
+  
+  constructor(){
+    this.firsName = '';
+    this.emailId = '';
+    this.date = '';
+    this.city = '';
+    this.mobileNumber = 0;
+  }
 }
