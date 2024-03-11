4eb9f860d27dd10ce1eed9e03ffe30806e3f6ab7
VDRohit <sapkalrohit110@gmail.com>
2024-03-11 19:22:16 +0530

    perform event binding for doctor registartion and save data successfully with created rest point

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -30,14 +30,36 @@ export class LoginComponent {
     })
 
   }
+
+  doRegistration(){
+    this.http.post('http://localhost:8081/doctor/register', this.loginObj).subscribe((res: any) => {
+      console.log(res);
+      if (res.result) {
+        alert(res.message);
+        this.router.navigateByUrl("/dashboard")
+      } else {
+        alert(res.message)
+      }
+    })
+  }
 }
 
 export class Login {
   userName: string;
   passWord: string;
+  name: string;
+  cell_no: number;
+  sector: string;
+  gender: string;
+  address: string;
 
   constructor() {
     this.userName = '';
     this.passWord = '';
+    this.name = '';
+    this.cell_no = 0;
+    this.sector = '';
+    this.gender = '';
+    this.address = '';
   }
 }
