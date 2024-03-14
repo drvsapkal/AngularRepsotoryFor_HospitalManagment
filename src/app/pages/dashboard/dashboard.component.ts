7f8834707d3c4b2747b8bad89ca690d56aef0d94
VDRohit <sapkalrohit110@gmail.com>
2024-03-14 19:34:26 +0530

    Fetch the patients details based on component initialization at first

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.getPatientsDetails();
+  }
+
+  Patients: any[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  getPatientsDetails(): void {
+    const url = 'http://localhost:8081/patient/details';
+    this.http.get<any[]>(url).subscribe((data: any[]) => {
+      this.Patients = data;
+    });
+  }
 
 }
