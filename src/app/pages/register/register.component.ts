e68217c7a92b9452c47ae08f7d8fe4c2402d0d2d
VDRohit <sapkalrohit110@gmail.com>
2024-03-14 19:35:50 +0530

    Changes to add Patients Details

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,12 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -12,22 +14,35 @@ export class RegisterComponent {
 
   patientobj : patient
 
-  constructor(){
+  constructor(private http:HttpClient, private router: Router){
     this.patientobj = new patient();
   }
+
+  bookAppointment(){
+    this.http.post('http://localhost:8081/patient/add', this.patientobj).subscribe((res: any) => {
+      console.log(res);
+      if (res.result) {
+        alert(res.message);
+        this.router.navigateByUrl("/dashboard")
+      } else {
+        alert(res.message)
+      }
+    })
+  }
+
 }
 
 export class patient {
   firsName: string;
   emailId: string;
-  date: string;
+  date: Date;
   city: string;
   mobileNumber: number;
   
   constructor(){
     this.firsName = '';
     this.emailId = '';
-    this.date = '';
+    this.date = new Date;
     this.city = '';
     this.mobileNumber = 0;
   }
